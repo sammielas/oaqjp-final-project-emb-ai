70c2618acee41f829194f0b68fe96c225246e124
sammielas <samflicks@gmail.com>
2025-02-17 05:18:28 -0500

    add error handling

--- server.py
+++ server.py
@@ -6,15 +6,13 @@ app = Flask(__name__)
 @app.route('/emotionDetector')
 def emotion_detector_route():
     text_to_analyze = request.args.get('textToAnalyze')
-    print("Input Text:", text_to_analyze)  # Debugging statement
-    if not text_to_analyze:
+    if not text_to_analyze or not text_to_analyze.strip():
         return "Invalid text! Please try again!"
-    
+
     response = emotion_detector(text_to_analyze)
-    print("API Response:", response)  # Debugging statement
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    
+
     return f"For the given statement, the system response is {response}."
 
 @app.route('/')
